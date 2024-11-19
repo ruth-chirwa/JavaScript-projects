a51942c9190fb0578f7e68ebca1aeaec5b9ce69c
ruth-chirwa <chirwaruth647@gmail.com>
2024-11-19 04:29:51 -0500

    new exercise

---
+++ exercises/index.js
@@ -0,0 +1,20 @@
+let girlsNames = ["Ruth", "Esther", "Alice", "Sana", "Omega"]
+console.log(girlsNames);
+
+
+//exercise 2
+
+girlsNames.push("Miracle"); 
+console.log(girlsNames); 
+
+
+//exercise 3
+
+girlsNames.shift("Ruth");
+console.log(girlsNames);
+
+
+//exercise 4 
+
+girlsNames.sort();
+console.log(girlsNames);
